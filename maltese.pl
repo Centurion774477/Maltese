6e76576cb9720fdab6358375c47cf678510ec410
Centurion774477 <reeceturner1358@gmail.com>
2026-07-06 18:32:09 -0400

    pretty good

--- maltese.pl
+++ maltese.pl
@@ -2,11 +2,14 @@
 use strict; use warnings;
 use feature 'say';
 use JSON::Tiny;
+use Path::Tiny;
+
 # CLI based, concise version coming later
 
 # Maltese is MacOS oriented
 
-my $saveFile = path(malteseSave.jsonl);
+my $saveFile = path("malteseSave.jsonl");
+my $userFile = path("malteseUser.pl");
 my $data = {};
 sub create {
     GETNAME:
@@ -16,17 +19,35 @@ sub create {
     my $got = <STDIN>; chomp($got);
     goto GETNAME unless $got eq "Y";
 
-    $data->{packageName} = $nameGiven; # its a bit unsafe but faster
+    $data->{packageName} = $nameGiven;
     $data->{scripts} = [];
     say "Package successfully created. Remember to save before closing Maltese.";
+}
+
+# argue @scriptArray as... an array of scripts? Shocker. . .
+sub parseScripts {
+        for my $script (@scriptArray) {
+            # brew -> brew install elixir
+            if ($script =~ /(?<header>\w+)\s+->\s+(?<content>.+/) {
+                my $hash = {scriptName => $+{header}, scriptBody => $+{content}};
+                if ($+{header} =~ /sudo/) {
+                    die 'Sorry, but Maltese does not allow sudo commands.'; # eventually, add more validation beyond the sudo checker
+                }
+                push($data->{scripts}), $hash;
+            }
+            else {
+                die 'Invalid script found:' . $script
+                # maybe make a better error check later
+            }
+        }
 }
 
 sub add {
     GETNAME:
     say "Start by naming this script; maybe something like 'brewElixir'?";
-    my $scriptName = <STDIN>;
+    chomp(my $scriptName = <STDIN>;)
     say "Confirm name:" . $scriptName . "Type Y to approve; anything else will disapprove.";
-    my $got = <STDIN>; chomp($got); goto GETNAME unless $got eq "Y";
+    chomp(my $got = <STDIN>); goto GETNAME unless $got eq "Y";
 
     say "
     Now provide your scripts.
@@ -39,50 +60,66 @@ sub add {
     - end with 'END'
     ";
     my $index = 0;
-    @scriptArray # each value is an individual script
+    my @scriptArray # each value is an individual script
     while (my $input = <STDIN>) {
         $index += 1;
         chomp($input);
-        push($input), @scriptArray;
+        push @scriptArray, $input;
         last if $input eq 'END';
         if ($index > 19) {
             say "You forgot your 'END'. Here's another try.";
             redo;
         }
     }
-    my sub parseScripts {
-        for my $script (@scriptArray) {
-            # brew -> brew install elixir
-            if ($script =~ /(?<header>\w)\s+->\s+(?<content>.)/) { # Nesting!!!
-                my $hash = {scriptName => $+{header}, scriptBody => $+{content}};
-                push($data->{scripts}), $hash;
-            }
-            else {
-                die 'Invalid script found:' . $script
-                # maybe make a better error check later
-            }
-        }
-    }
+    parseScripts()
 }
 
 sub save {
     # push the temporary $data object to jsonl (or something else) then clear it
     my $count = 0;
     $count = @{$data->{scripts}};
-    if ($count == 0) {die 'You cannot save right now --you havent given Maltese any scripts.'};
+    if ($count == 0) {die 'You cannot save because you havent given Maltese any scripts.'};
     $saveFile->append(encode_json($data->{scripts}) . "\n");
     $data->{scripts} = [];
-    
-    say "Your package has been saved."
+    say "Your package has been saved.";
 }
 
 sub readSave {
-    say "reading!";
-    my @scripts = map {decode_json($_)};
+    # I have no fucking clue how to parse JSON
+    my @scripts = map {decode_json($_)} $saveFile->lines({chomp => 1});
+    die 'You dont have any scripts for this project' if (scalar @{data->{scripts}} == 0); # Trying to check for an empty array
+    for my $script (@scripts) {
+        push@{$data->{scripts}}, $script;
+    }
+    say "Successfully restored your previous session.";
 }
 
+# I don't literally want to compile the entire Maltese project; we only need a reader version.
+# I'll probably just finalize the data in jsonl, then package that with a lightweight script.
+# Either way: get the output working and the user side can be ANYTHING
+# if this doesn't work I'll just compile the full Maltese file.
 sub compile {
+    unless (defined $data->{packageName}) {
+        die 'Please define a project before compiling'
+    }
     say "compiling!";
+    say "Confirm: You are about to pack your scripts for" . $data->{packageName} . "Are you sure? Y/N";
+    chomp(my $gets = <STDIN>);
+    $gets eq 'Y' ? say 'compiling . . .' : say 'Abandoning the compilation process' && return;
+    if ($gets eq 'Y') {
+        say 'compiling . . .';
+    } else {
+        say 'Abandoning the compilation process';
+        return;
+    }
+    # now we have to run some scripts
+    # first, package the jsonl and malteseUser.pl files into a single directory
+    
+}
+
+# this really doesn't need to be a function but it's cleaner this way
+sub exit {
+    say "exiting!";
 }
 
 do {
@@ -94,10 +131,11 @@ do {
     3 -> Compile a package
     4 -> Save package without compiling
     5 -> Pick back up from a save
+    6 -> Exit Maltese
     ";
-    my $input = <STDIN>;
+    chomp(my $input = <STDIN>);
     my @options = qw[1 2 3]
-    unless ($input ~~ @options) {
+    unless (grep {$_ eq $input} @options)) {
         say 'Invalid input; only enter 1, 2, or 3.';
         redo;
     }
@@ -113,8 +151,15 @@ do {
     elsif ($input eq '4') {
         save();
     }
-    else {
-        say 'invalid input'; redo; # this won't work, will it? Since it's in its own nest
+    elsif ($input eq '5') {
+        readSave();
     }
-}
+    elsif ($input eq '6') {
+        exit();
+    }
+    else {
+        say 'invalid input';
+        continue;
+    }
+} while (1)
 
